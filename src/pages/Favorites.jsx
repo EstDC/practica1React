5fa5e37654fa5d14b77199b8887c75693f77ff86
EstDC <edc.modulo.fp@gmail.com>
2024-12-30 15:03:02 +0100

    Corrección del import Footer en Favorites.

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NewsCard from '../components/NewsCard';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
-import Footer from '../Footer';
+import Footer from '../components/Footer';
 
 function Favorites({ favoritos, toggleFavorito }) {
     return (
