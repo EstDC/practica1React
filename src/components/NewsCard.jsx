54ab03ca4df484b4cf1f21c890897337d3e99648
EstDC <edc.modulo.fp@gmail.com>
2024-12-30 16:31:05 +0100

    Probando otra verificción del evento onclick de favoritos.

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -5,19 +5,36 @@ console.log('El archivo Card.jsx se ha cargado')//Comprobación
 
 function Card({ id, headline, date, body, section, image_url, author, translations, toggleFavorito, favoritos }) {
   //Comprobación de que se está cargando correctamente los datos
-  console.log('Props recibidas en Card:', { id, headline, favoritos });
+  console.log('Props recibidas en Card:', { id, headline, favoritos, toggleFavorito });
   useEffect(() => {
     console.log('El componente Card se montó con ID:', id);
   }, [id]);
-  const handleClick = () => {
-    console.log('Clic en la tarjeta con ID:', id);
-  };
+  //const handleClick = () => {
+    //console.log('Clic en la tarjeta con ID:', id);
+ // };
   //(Todo lo anteriro se puede borrar si se quiere, pero así puedo comprobar el correcto funcionamiento)
   //--------------------------------------------------------------------
 
    //Verificar si el artículo es favorito
    const isFavorito = Array.isArray(favoritos) && favoritos.some((item) => item.id === id);
    console.log('Botón de Favoritos clicado con ID:', id);
+   const handleClick = () => {
+    console.log('Botón de Favoritos clicado con ID:', id); //Verifica que el evento se dispare
+    if (typeof toggleFavorito === 'function') {
+      toggleFavorito({
+        id,
+        headline,
+        date,
+        body,
+        section,
+        image_url,
+        author,
+        translations
+      });
+    } else {
+      console.error('Error: toggleFavorito no es una función');
+    }
+  };
   
   const { language } = useContext(LangContext); // Obtener idioma actual
   const translation = translations?.[language] || {};// Buscar la traducción
@@ -29,16 +46,7 @@ function Card({ id, headline, date, body, section, image_url, author, translatio
       <div className="card">
         <button 
           className={`favorito-button ${isFavorito ? 'active' : ''}`}
-          onClick={() => toggleFavorito({
-            id,
-            headline,
-            date,
-            body,
-            section,
-            image_url,
-            author,
-            translations
-          })}
+          onClick={handleClick}
         >
           {isFavorito ? '⭐' : '☆'}
         </button>
